f6e2063486fa373ed51029d56d545f1a35724c90
Vincent Pretre <vincent.pretre@gmail.com>
2023-01-10 15:06:32 +0100

    Add husky/prettier

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # @smartesting/flaky-test-detector
 
 KISS approach for flaky test detection:
- - run the tests `n` times
- - check the results
- - find tests which do not have consistent results
+
+- run the tests `n` times
+- check the results
+- find tests which do not have consistent results
--- sample-suite/samples.test.ts
+++ sample-suite/samples.test.ts
@@ -1,16 +1,16 @@
-import assert from 'assert'
+import assert from "assert";
 
-describe('Sample suite', () => {
-  it('is a stable test', () => {
-    assert(true)
-  })
+describe("Sample suite", () => {
+  it("is a stable test", () => {
+    assert(true);
+  });
 
-  it('is a stable test (although it fails)', () => {
-    assert(false)
-  })
+  it("is a stable test (although it fails)", () => {
+    assert(false);
+  });
 
-  it('is a flaky test', () => {
-    const rand = Math.random()
-    assert(rand >= 0.5)
-  })
-})
+  it("is a flaky test", () => {
+    const rand = Math.random();
+    assert(rand >= 0.5);
+  });
+});
