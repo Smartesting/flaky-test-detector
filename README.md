904f61b4970af30387af2aa1db2efee990ac8ada
Vincent Pretre <vincent.pretre@gmail.com>
2023-01-10 15:04:39 +0100

    Bootstrap project

---
+++ README.md
@@ -0,0 +1,6 @@
+# @smartesting/flaky-test-detector
+
+KISS approach for flaky test detection:
+ - run the tests `n` times
+ - check the results
+ - find tests which do not have consistent results
---
+++ sample-suite/samples.test.ts
@@ -0,0 +1,16 @@
+import assert from 'assert'
+
+describe('Sample suite', () => {
+  it('is a stable test', () => {
+    assert(true)
+  })
+
+  it('is a stable test (although it fails)', () => {
+    assert(false)
+  })
+
+  it('is a flaky test', () => {
+    const rand = Math.random()
+    assert(rand >= 0.5)
+  })
+})
