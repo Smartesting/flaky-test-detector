0e8a89da642e7c7e2b84378390d438e4f513b027
Christophe Grandpierre <c.grandpierre@smartesting.com>
2023-07-20 11:28:48 +0200

    redirect testRun process stdout/err to main process ones

--- src/commandFactory.ts
+++ src/commandFactory.ts
@@ -6,6 +6,8 @@ const commandFactory: CommandFactory = (options, reportParser) => {
   return async () => {
     return new Promise<ReadonlyArray<TestExecution>>((resolve) => {
       const testRun = exec(options.runTests)
+      testRun.stdout?.pipe(process.stdout)
+      testRun.stderr?.pipe(process.stderr)
 
       testRun.on('exit', () => {
         const report = readFileSync(options.testOutputFile, 'utf-8')
